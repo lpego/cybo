e03c3e3b9f977886ae2c700bfc57edbe68eff4fb
Luca Pegoraro <luca.pegoraro90@gmail.com>
2024-05-06 11:40:48 +0200

    Fixed multiline HTML parsing, special characters, and tidied up script

--- webinar_invitation.py
+++ webinar_invitation.py
@@ -4,67 +4,32 @@ import html
 from OAuth2_email_test import send_email
 
 ### Provide filename of the PARSED WordPress export here
-filename = "cybo-webinars-2024-registration-2024-04-15_14 06 53_parsed.csv"
+filename = "cybo-webinars-2024-registration-2024-05-02_17 50 33_parsed.csv"
 
 ### Provide surname of speaker here
 speaker = "Riva"
 
-### Paste the Zoom webinar invite here
-# # could be opened from a txt file
-# with open("D:\cybo_emails\zoom_riva.txt", "rb") as file: 
-#     zoom_invite = file.read()
+### Paste the Zoom webinar invite here; 
+### could be opened from a txt file (more secure)...
+with open("D:\cybo_emails\zoom_riva.txt", "r") as file: 
+    zoom_invite = file.read()
 
-# # replacement strings because we need to fix the EoL...
-# WINDOWS_LINE_ENDING = b'\r\n'
-# UNIX_LINE_ENDING = b'\n'
-# HTML_LINE_ENDING = b'<br/'
-# print(zoom_invite.replace(WINDOWS_LINE_ENDING, HTML_LINE_ENDING))
+# ### ...or hardcoded with multiline string (might be useful for debugging)
+# zoom_invite = """
+# Someone is inviting you to a scheduled Zoom meeting.
 
-# or hardcoded with multiline string
-zoom_invite = r"""Luca Pegoraro is inviting you to a scheduled Zoom meeting.
+# Topic: CYBO Webinar - Federico Riva
+# Time: May 10, 2024 14:30 Zurich
 
-Topic: CYBO Webinar - Federico Riva
-Time: May 10, 2024 14:30 Zurich
+# Join Zoom Meeting
+# https://example.link.us/
 
-Join Zoom Meeting
-https://wsl.zoom.us/j/67839727760?pwd=V2lXRVZFeUVzL1ZmSjkvQzMvS0IrZz09
+# Meeting ID: 678 3972 7760
+# Passcode: 634406
 
-Meeting ID: 678 3972 7760
-Passcode: 634406
+# [...]
 
----
-
-One tap mobile
-+31207947345,,67839727760# Netherlands
-+31707006526,,67839727760# Netherlands
-
----
-
-Dial by your location
-• +31 20 794 7345 Netherlands
-• +31 707 006 526 Netherlands
-• +31 20 241 0288 Netherlands
-• +31 20 794 0854 Netherlands
-• +31 20 794 6519 Netherlands
-• +31 20 794 6520 Netherlands
-
-Meeting ID: 678 3972 7760
-
-Find your local number: https://wsl.zoom.us/u/cbv15JBlX6
-
----
-
-Join by SIP
-• 67839727760@fr.zmeu.us
-
----
-
-Join by H.323
-• 213.19.144.110 (Amsterdam Netherlands)
-• 213.244.140.110 (Germany)
-
-Meeting ID: 678 3972 7760
-Passcode: 634406"""
+# """
 
 ### Get emails for attendees of the specified webinar
 data = pd.read_csv(filename, skipinitialspace=True) 
@@ -75,11 +40,24 @@ email_addresses = ["luca.pegoraro@outlook.com"#, "maria.guerrina@edu.unige.it", 
 
 ### Send emails
 for addressee in email_addresses: 
-    # print(addressee, f"{pdf_path}\split\{certificate}")
+    print("Sending email to: ", addressee)
     send_email(
-        sender="conferenceyoungbotanists@gmail.com", 
+        sender="conferenceyoungbotanists@gmail.com",
         to=addressee,
-        subject="CYBO test webinar invitation email 5", 
-        msgHtml=f"Dear webinar participant,<br/>Please find below the Zoom meeting invite for the second CYBO webinar on May 10, 2024.<br/><br/>We hope to see you then!<br/>Kind regards,<br/>the CYBO steering committee.<br/><br/>{html.escape(zoom_invite)}<br/>", 
-        msgPlain=f"Dear webinar participant,\nPlease find below the Zoom meeting invite for the second CYBO webinar on May 10, 2024.\n\nWe hope to see you then!\nKind regards,\nthe CYBO steering committee.\n\n{zoom_invite}\n"
+        subject="Plain text to HTML parsing test",
+        msgHtml="""
+        HTML here.<br/>
+        Dear webinar participant,<br/>
+        find below the Zoom link to the next CYBO webinar.<br/>
+        <br/>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<br/>
+        <br/>
+        """ + html.escape(zoom_invite).replace('\n', '<br/>'),
+        msgPlain="""
+        Plain text here.\n
+        Dear webinar participant,\n
+        find below the Zoom link to the next CYBO webinar.\n
+        \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
+        \n
+        """ + html.unescape(zoom_invite),
+        # attachmentFile="Webinar_10May_Riva-compressed.pdf" # check if attachment is needed
         )
