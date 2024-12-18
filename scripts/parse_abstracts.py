d5527efc9d1aee4996259955f6b2ea61ba6a9cd1
Luca Pegoraro <luca.pegoraro90@gmail.com>
2024-12-18 11:59:23 +0100

    Now reads most recent database versions automatically

--- scripts/parse_abstracts.py
+++ scripts/parse_abstracts.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import re
 import os
+from glob import glob
+from latest_file import find_most_recent_file
 
-### Provide file name here
-filename = "..\website_exports\cybo-2025-registration,-with-contribution-2024-12-09_16_18_43.csv"
+### Provide file lists here here
+filelist = glob("..\website_exports\cybo-2025-registration,-with-contribution*")
+filename = find_most_recent_file(filelist) # grab most recent version
+print("Reading from file: ", filename)
 
 data = pd.read_csv(filename)
 
@@ -12,6 +16,7 @@ data = data.apply(lambda x: str(x).replace('\n', ' ') if isinstance(x, str) else
 
 ### count preferred sessions
 data['Preferred session'].value_counts()
+### gender balance
 data['Gender'].value_counts()
 
 ### Prepare reduced version
@@ -21,12 +26,15 @@ data_redux = data[['First Name', 'Last Name', 'Email address', 'Career stage', '
 # data_redux["Username"] = data['Email address'].str.extract(r'^([^@]+)@')
 
 ### Grab full user table
-wp_users = pd.read_csv("..\website_exports\wp_users_10Dec2024.csv")
+# wp_users = pd.read_csv("..\website_exports\wp_users_10Dec2024.csv")
+wp_users = pd.read_csv(find_most_recent_file(glob("..\website_exports\wp_users*")))
+print("Reading user table from file: ", find_most_recent_file(glob("..\website_exports\wp_users*")))
 ### Merge by email
 data_redux = data_redux.merge(wp_users[['user_email', 'user_login']], left_on='Email address', right_on='user_email', how='left')
 ### Make URL
 data_redux["URL"] = 'https://conferenceyoungbotanists.com/abstracts/' + data_redux['user_login']
 
 ### write out to file
+print("Writing out: ", os.path.splitext(filename)[0]+"_parsed.csv", " ; ", os.path.splitext(filename)[0]+"redux.csv")
 data.to_csv(os.path.splitext(filename)[0]+"_parsed.csv")
-data_redux.to_csv("..\\website_exports\\abstracts_redux_parsed.csv")
+data_redux.to_csv(os.path.splitext(filename)[0]+"redux.csv")
