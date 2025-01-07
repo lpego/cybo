5a3e14dbd722a97dde150ce4b69d907285111216
Luca Pegoraro <luca.pegoraro90@gmail.com>
2025-01-07 12:03:32 +0100

    Added latest file grabbing and checked

--- scripts/merge_abstracts_evaluations.py
+++ scripts/merge_abstracts_evaluations.py
@@ -1,14 +1,33 @@
+### This script merges the abstracts and reviewers' evaluations
+
+# %% 
 import pandas as pd
 import re
 import os
+from glob import glob
+from latest_file import find_most_recent_file
 
-### Provide file name here
-abstracts = pd.read_csv("..\\website_exports\\abstracts_redux_parsed.csv")
-evaluations = pd.read_csv("..\\website_exports\\evf-entry-export-abstract-evaluation-2024-12-11-9622391a024e2387a88157a78094f283.csv")
+# %% Read in most recent version of abstracts evaluations (i.e. Dashboard > Everest Forms > Entries > Export CSV)
+filelist = glob("..\website_exports\evf-entry-export-abstract-evaluation-*.csv")
+filename = find_most_recent_file(filelist) # grab most recent version
+print("Reading from file: ", filename)
+evaluations = pd.read_csv(filename)
 evaluations = evaluations.drop(["Date Created", "User Device", "User IP Address"], axis=1)
 
+# %% Read in most recent version of abstracts (i.e. Dashboard > User Registration > Settings > Import/Export > Export users)
+filelist = glob("..\website_exports\cybo-2025-registration,-with-contribution*[_redux].csv")
+filename = find_most_recent_file(filelist) # grab most recent version
+print("Reading from file: ", filename)
+abstracts = pd.read_csv(filename)
+abstracts = abstracts.drop("Unnamed: 0", axis=1)
+
+# %%
 ### Merge by URL
 merged = abstracts.merge(evaluations, left_on="URL", right_on="Link to the abstract", how="left")
+merged = merged.drop("Link to the abstract", axis=1)
 
+# %%
 ### write out to file
 merged.to_csv("..\website_exports\merged_abstracts_evaluations.csv")
+
+# %%
