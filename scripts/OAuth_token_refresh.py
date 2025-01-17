0615c95ccdc5b32bcb86f7b5efa0a567000f211f
Luca Pegoraro <luca.pegoraro@wsl.ch>
2025-01-17 15:41:55 +0100

    script to refresh OAuth token if expired

---
+++ scripts/OAuth_token_refresh.py
@@ -0,0 +1,18 @@
+import html
+from OAuth2_email_test import SendMessage
+### Hardcoded arguments    
+def send_email():
+    """Send email with attachment"""
+    to = "luca.pegoraro@outlook.com"
+    cc = "luca.pegoraro@wsl.ch"
+    sender = "conferenceyoungbotanists@gmail.com"
+    subject = "CYBO emails - testing PDF attachments"
+    msgHtml = "Hi<br/>Html Email"
+    msgPlain = "Hi\nPlain Email"
+    ### Send message without attachment: 
+    # SendMessage(sender, to, subject, msgHtml, msgPlain)
+    ### Send message with attachment: 
+    SendMessage(sender, to, cc, subject, msgHtml, msgPlain, 'D:\cybo\webinars_cybo2024_v4.1_compressed.pdf')
+
+if __name__ == '__main__':
+    send_email()
