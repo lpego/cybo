3e46049cfa4629a0c675e2f4201629e49ce45eaf
Luca Pegoraro <luca.pegoraro90@gmail.com>
2025-02-19 11:11:55 +0100

    Re-sending corrected attendance certificates to sub-selection of participants...

--- scripts/send_conference_attendance_certificates.py
+++ scripts/send_conference_attendance_certificates.py
@@ -13,15 +13,26 @@ participants = pd.read_csv("..\\website_exports\\presences_for_certificates.csv"
 # %% Read in award winners
 winners = pd.read_csv("..\\website_exports\\awards_for_certificates.csv")
 
+# %% wrong certificates, to re-send
+with open("..\\website_exports\\certificates_to_resend.txt", "r") as textfile: 
+    resend = textfile.read().splitlines()
+    # print(resend)
+### Filter participants to re-send certificates
+resend_participants = participants[participants['Name'].isin(resend)]
+
 # # %% testing loop
-# for name, email in participants[['Name', 'Email address',]].values:
-#     print(name, email)
+# # for name, email, contribution, affiliation in participants[['Name', 'Email address', 'Contribution', 'Institution']].values:
+# for name, email, contribution, affiliation in resend_participants[['Name', 'Email address', 'Contribution', 'Institution']].values:
+#     print(name, "   ", email)
+#     print("Contribution type: ", contribution)
+#     print("Affiliation: ", affiliation)
 #     filename = "Attendance_certificate_CYBO2025_" + name.replace(" ", "_") + ".pdf"
 #     print("Does the file exist?     ", os.path.isfile("..\\certificates\\attendance_certificates\\" + filename))
 #     print("=====================================")
 
 # %% Send attendance certificate emails
-for name, email in participants[['Name', 'Email address',]].values:
+# for name, email in participants[['Name', 'Email address',]].values:
+for name, email in resend_participants[['Name', 'Email address',]].values:
     filename = "Attendance_certificate_CYBO2025_" + name.replace(" ", "_") + ".pdf"
     print("Sending email to: ", email)
     send_email(
@@ -33,21 +44,15 @@ for name, email in participants[['Name', 'Email address',]].values:
         subject="CYBO 2025 attendance certificate",
         msgHtml="""
         <p>Dear {name}, <br>
-        &nbsp; thank you for taking part in CYBO 2025, we hope you enjoyed the conference! <br><br>
-        
-        Please find attached your attendance certificate. <br><br>      
-        
-        We hope to see in future editions of CYBO! <br>
+        &nbsp; apologies for the mistake, please find attached your corrected attendance certificate. <br><br>
+                
         Kind regards, <br>
         CYBO organising committee.</p>
         """.format(name=name),
         msgPlain="""
         "Dear {name},\n
-           thank you for taking part in CYBO 2025, we hope you enjoyed the conference!\n\n
+           apologies for the mistake, please find attached your corrected attendance certificate. \n\n
            
-        Please find attached your attendance certificate.\n\n
-        
-        We hope to see in future editions of CYBO!\n
         Kind regards,\n
         CYBO organising committee.
         """.format(name=name),
@@ -61,38 +66,38 @@ for name, email in participants[['Name', 'Email address',]].values:
 #     print("Does the file exist?     ", os.path.isfile("..\\certificates\\award_certificates\\" + filename))
 #     print("=====================================")
 
-# %% Send award certificate emails
-for name, email in winners[['Name', 'Email address',]].values:
-    filename = "Award_certificate_CYBO2025_" + name.replace(" ", "_") + ".pdf"
-    print("Sending email to: ", email)
-    send_email(
-        sender="conferenceyoungbotanists@gmail.com",
-        ### normal addressees list
-        to=email,
-        # ### my emails for testing
-        # to="luca.pegoraro@wsl.ch",
-        subject="CYBO 2025 award certificate",
-        msgHtml="""
-        <p>Dear {name}, <br>
-        &nbsp; we hope you enjoyed CYBO 2025! <br><br>
+# # %% Send award certificate emails
+# for name, email in winners[['Name', 'Email address',]].values:
+#     filename = "Award_certificate_CYBO2025_" + name.replace(" ", "_") + ".pdf"
+#     print("Sending email to: ", email)
+#     send_email(
+#         sender="conferenceyoungbotanists@gmail.com",
+#         ### normal addressees list
+#         to=email,
+#         # ### my emails for testing
+#         # to="luca.pegoraro@wsl.ch",
+#         subject="CYBO 2025 award certificate",
+#         msgHtml="""
+#         <p>Dear {name}, <br>
+#         &nbsp; we hope you enjoyed CYBO 2025! <br><br>
         
-        Please find attached your award certificate. <br><br>
+#         Please find attached your award certificate. <br><br>
         
-        We hope to see in future editions of CYBO! <br>
-        Kind regards, <br>
-        CYBO organising committee.</p>
-        """.format(name=name),
-        msgPlain="""
-        "Dear {name},\n
-           we hope you enjoyed CYBO 2025!\n\n
+#         We hope to see in future editions of CYBO! <br>
+#         Kind regards, <br>
+#         CYBO organising committee.</p>
+#         """.format(name=name),
+#         msgPlain="""
+#         "Dear {name},\n
+#            we hope you enjoyed CYBO 2025!\n\n
            
-        Please find attached your award certificate.\n\n
+#         Please find attached your award certificate.\n\n
         
-        We hope to see in future editions of CYBO!\n
-        Kind regards,\n
-        CYBO organising committee.
-        """.format(name=name),
-        attachmentFile="..\\certificates\\award_certificates\\" + filename
-        )
+#         We hope to see in future editions of CYBO!\n
+#         Kind regards,\n
+#         CYBO organising committee.
+#         """.format(name=name),
+#         attachmentFile="..\\certificates\\award_certificates\\" + filename
+#         )
 
 # %%
