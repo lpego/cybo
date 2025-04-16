2717fea61d04a22c8409173a3d59479f0029e8af
Luca Pegoraro <luca.pegoraro90@gmail.com>
2025-04-16 15:10:54 +0200

    Modified to use presence list only, not all collected emails

--- scripts/send_emails_generic.py
+++ scripts/send_emails_generic.py
@@ -7,14 +7,11 @@ import html
 from bs4 import BeautifulSoup
 from OAuth2_email import send_email
 
-### get email list 
-with open("..\website_exports\email_list.txt") as f:
-     email_addresses = [line.rstrip('\n') for line in f]
-# print(type(email_addresses))
-# print(email_addresses)
+### Read in conference participants
+participants = pd.read_csv("..\\website_exports\\presences_for_certificates.csv")
 
 ### Read the HTML content from an external file
-with open("..\emails\email_content.html", "r", encoding="utf-8") as file:
+with open("..\\emails\\abstract_correction_photos.html", "r", encoding="utf-8") as file:
     msgHtml = file.read()
 
 ### Convert HTML to plain text
@@ -26,16 +23,15 @@ subject_tag = soup.find("meta", {"name": "email-subject"})
 subject = subject_tag["content"] if subject_tag else "No Subject"
 
 ### Send emails
-for addressee in email_addresses:
+for addressee in participants["Email address"]:
     print("Sending email to: ", addressee)
     send_email(
         sender="conferenceyoungbotanists@gmail.com",
-        # to=addressee,
-        ### my emails for testing
-        to="luca.pegoraro@outlook.com",
-        cc="luca.pegoraro@wsl.ch", 
+        to=addressee,
+        # ### my emails for testing
+        # to="luca.pegoraro@outlook.com",
+        # cc="luca.pegoraro@wsl.ch", 
         subject=subject,
         msgHtml=msgHtml,
         msgPlain=msgPlain,
-        # attachmentFile="Webinar_10May_Riva-compressed.pdf" # check if attachment is needed
     )
