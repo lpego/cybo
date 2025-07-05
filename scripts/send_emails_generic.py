7230aec41f8895f304fea020e1e20bb59f3295a0
Luca Pegoraro <luca.pegoraro@wsl.ch>
2025-07-05 10:07:41 +0200

    Sending emails for book of abstracts publication

--- scripts/send_emails_generic.py
+++ scripts/send_emails_generic.py
@@ -8,10 +8,11 @@ from bs4 import BeautifulSoup
 from OAuth2_email import send_email
 
 ### Read in conference participants
-participants = pd.read_csv("..\\website_exports\\presences_for_certificates.csv")
+with open("..\\website_exports\\email_list.txt") as f:
+     email_addresses = [line.rstrip('\n') for line in f]
 
 ### Read the HTML content from an external file
-with open("..\\emails\\abstract_correction_photos.html", "r", encoding="utf-8") as file:
+with open("..\\emails\\email_bookofabstracts.html", "r", encoding="utf-8") as file:
     msgHtml = file.read()
 
 ### Convert HTML to plain text
@@ -23,7 +24,7 @@ subject_tag = soup.find("meta", {"name": "email-subject"})
 subject = subject_tag["content"] if subject_tag else "No Subject"
 
 ### Send emails
-for addressee in participants["Email address"]:
+for addressee in email_addresses:
     print("Sending email to: ", addressee)
     send_email(
         sender="conferenceyoungbotanists@gmail.com",
